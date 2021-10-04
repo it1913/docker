439f095e7f1e25f4d1aea8bf4c9163307473bbac
it1913 <66723368+it1913@users.noreply.github.com>
2021-10-04 12:15:26 +0200

    docker_2

--- it3prgapp/script.py
+++ it3prgapp/script.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, url_for
+
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 @app.route("/")
 def ahoj():
@@ -10,3 +12,18 @@ def ahoj():
 def hello(name=None):
     pole=["jedna","dvě","tři"]
     return render_template('script.html', pole=pole, name=name)
+
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, FloatField
+
+
+class RegistrationForm(Form):
+    a = FloatField('Value a', [validators.number_range(0,10)])
+    b = FloatField('Value b', [validators.number_range(0,10)])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('Thanks for registering')
+        return str(int(form.a.data) + int(form.b.data))
+    return render_template('register.html', form=form)
